ec379d2da296535d7fd0eda4c08210c866541190
IgorSulj <igorsuljin@gmail.com>
2024-03-21 09:07:35 +0300

    Fixed formatting

--- Code1.py
+++ Code1.py
@@ -1,29 +1,37 @@
 num = int(input("Введите значение: "))
-#декоратор cash, который применяется к функции func
+
+
+# декоратор cash, который применяется к функции func
 def cash(func):
-    #считает количество вызовов нашей функции
+    # считает количество вызовов нашей функции
     counter = 0
     cash = func()
+
     def deco():
         # проверяется значение counter, если оно меньше или =3, то значение
-        #cash возращается
+        # cash возращается
         nonlocal counter
         counter += 1
         if counter <= 3:
             return cash
-        
+
         else:
             counter = 1
             return func()
-    return deco                     
+
+    return deco
+
+
 @cash
 def func():
-    #умножает наше число на 100
-    x=num * 100
-    print('Кеш')
+    # умножает наше число на 100
+    x = num * 100
+    print("Кеш")
     return x
-#я написала свой коммантарий. Катюня
-print(func())                      
+
+
+# я написала свой коммантарий. Катюня
+print(func())
 print(func())
 
 print(func())
