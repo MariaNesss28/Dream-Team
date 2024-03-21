0f416b7e3e9eb3d646dae797f2f239af30f48fdb
MariaNesss28 <olga.nester.64@mail.ru>
2024-03-21 08:52:17 +0300

    Initial commit

---
+++ Code1.py
@@ -0,0 +1,30 @@
+num = int(input("Введите значение: "))
+#декоратор cash, который применяется к функции func
+def cash(func):
+    #считает количество вызовов нашей функции
+    counter = 0
+    cash = func()
+    def deco():
+        # проверяется значение counter, если оно меньше или =3, то значение
+        #cash возращается
+        nonlocal counter
+        counter += 1
+        if counter <= 3:
+            return cash
+        
+        else:
+            counter = 1
+            return func()
+    return deco                     
+@cash
+def func():
+    #умножает наше число на 100
+    x=num * 100
+    print('Кеш')
+    return x
+
+print(func())                      
+print(func())
+
+print(func())
+
